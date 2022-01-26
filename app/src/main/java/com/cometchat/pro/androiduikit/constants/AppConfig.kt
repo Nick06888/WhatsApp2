6504f1ea49b0a464e37532e071c027c80faa15e6
Nick06888 <84736159+NickQuelloVero@users.noreply.github.com>
2022-01-26 17:05:56 +0100

    first commit

---
+++ app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.kt
@@ -0,0 +1,9 @@
+package com.cometchat.pro.androiduikit.constants
+
+class AppConfig {
+    object AppDetails {
+        const val APP_ID = "XXXXXXXXXXXXXXXXX"
+        const val AUTH_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        const val REGION = "XX"
+    }
+}
